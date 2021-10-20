2f92e056a87196ede6f4e95389ea47bd2911388d
Bart PC <bbart318@gmail.com>
2021-10-20 00:01:41 -0500

    Fix updateLibrary() and add resetLibrary()

--- app.js
+++ app.js
@@ -1,44 +1,65 @@
-let myLibrary = [
-  {
-    title: "Illustrated Man",
-    author: "Ray Bradbury",
-    pages: 300,
-    read: false,
-  },
-  {
-    title: "Illustrated",
-    author: "Ray",
-    pages: 300,
-    read: true,
-  },
-  {
-    title: "Illustrated Woman",
-    author: "Ray Bradshaw",
-    pages: 200,
-    read: false,
-  },
-];
+const submitBtn = document.querySelector("#submit");
+const cardGrid = document.querySelector(".card-grid");
+
+let myLibrary = [];
 
 function Book(title, author, page, read) {
   this.title = title;
   this.author = author;
   this.page = page;
   this.read = read;
-  this.info = function () {
-    return `${title}, ${author}, ${page}, ${read}`; //
-  };
 }
-const theHobbit = new Book("The Hobbit", "by J.R.R Tolkien", "295 pages", "not read yet");
-console.log(theHobbit.info());
 
-function addBookToLibrary() {
-  let myBook = {
-    title: prompt("Title?", ""),
-    author: prompt("Author?", ""),
-    pages: prompt("Pages?", ""),
-    read: prompt("Have you read it?", ""),
-  };
-  let book = new Book();
-  return myLibrary.push(myBook);
+function BookFromInput() {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#read").checked;
+
+  return new Book(title, author, pages, read);
 }
-addBookToLibrary();
+
+function addBook(e) {
+  e.preventDefault();
+  const newBook = BookFromInput();
+  myLibrary.push(newBook);
+  myLibrary.slice(0, 1);
+  console.log(myLibrary);
+  createBookCard(newBook);
+  updateLibrary();
+}
+
+function updateLibrary() {
+  resetLibrary();
+  myLibrary.forEach((book, i) => {
+    const card = document.createElement("div");
+    const title = document.createElement("h2");
+    const author = document.createElement("h3");
+    const page = document.createElement("p");
+    const read = document.createElement("p");
+    title.textContent = myLibrary[i].title;
+    author.textContent = myLibrary[i].author;
+    page.textContent = myLibrary[i].page;
+    read.textContent = myLibrary[i].read;
+
+    card.classList.add("book-card");
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(page);
+    cardGrid.appendChild(card);
+  });
+}
+
+function resetLibrary() {
+  cardGrid.innerHTML = "";
+}
+
+function createBookCard() {}
+
+function addBookModal() {
+  const header = document.createElement("h2");
+
+  header.textContent = "Add a new book";
+}
+submitBtn.addEventListener("click", addBook);
+//
